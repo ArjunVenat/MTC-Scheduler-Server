7392f2beab6ce4a0a07a8747d1bc3e6309f55776
ArjunVenat <arjunvenat@gmail.com>
2023-12-24 10:43:20 -0500

    Excel_Parser completed solution almost there

--- Excel_Parser.py
+++ Excel_Parser.py
@@ -7,39 +7,42 @@ class Excel_Parser:
     
     def __init__(self, file_path):
         self.file_path = file_path
-        self.largeNum = np.inf #If the time slot is unavailable, the weight x_{ijk} will be this large number
-
+        self.large_num = 10000 #If the time slot is unavailable, the weight x_{ijk} will be this large number
         
-    def get_num_workers(self):
+    def get_all_data(self):
+        self.__parse_day_breakdown()
+        self.__get_workers_table()
+        self.__change_name_to_worker_id()
+        self.__update_workers_table()        
+        self.__get_preference_matrix()
+        return (self.workers_table, self.preference_matrix)
+        
+    def __get_num_workers(self):
         self.list_of_names = pd.read_excel(self.file_path, sheet_name="List of Names")
         self.list_of_names = self.list_of_names.iloc[1:, :] #removing heading
         return len(self.list_of_names)
 
-    def get_workers_table(self):
-        #I envision the workers table having the id, name, and position of the worker
-        #id is the index for the worker, like the indexed by i in the formulation
-        #can use np.where() to filter by position to get the necessary sets
-        
+    def __get_workers_table(self):
         toReturn = self.list_of_names.to_numpy()
         toReturn[:, 0] = np.array([int(toReturn[x, 0] - 1) for x in range(len(toReturn))])
         self.workers_table = toReturn
         
         return self.workers_table
     
-    def parse_day_breakdown(self):
+    def __parse_day_breakdown(self):
         #Some things here are hardcoded but these are for proof of concept rather than making it dynamic
         #More dynamic parsing is more suited for Qualtrics data than Excel
          
         start = 2 #Table starts at column B --> 2
         end = 11 #Table ends at column K --> 11
-        self.num_workers = self.get_num_workers()
+        self.num_workers = self.__get_num_workers()
         
         #Parse the Day Breakdown sheet
         df = pd.read_excel(self.file_path, sheet_name="Day Breakdown")
-        
         df = df.iloc[:, start-1: end] #1 based indexing means start-1, end is just end because splicing is and [inclusive, exclusive) interval
-        #Get all the index of all the empty rows and filter those out
-        empty_rows = df.index[df.isnull().all(axis=1)].tolist()  
+        
+        #Get all the indices of all the empty rows and filter those out
+        empty_rows = df.index[df.isnull().all(axis=1)].tolist()
         df_list = np.split(df, empty_rows)
         
         #Remove additional rows with all NaN values
@@ -49,46 +52,52 @@ class Excel_Parser:
         #Remove empty DataFrames from the list
         df_list = [d for d in df_list if not(d.empty)]
         
+        #Set parameters i
         self.num_shifts = len((df.iloc[:, 2:]).columns) #num shifts = number of columns save for the first 2
         self.num_days = len(df_list) #num days = length of df_list, as there are 5 preference tables for each day in the spreadsheet
         self.day_breakdown_list = df_list
 
-        
-        #Printing the output
-        #for df in self.day_breakdown_list:
-        #    print(df, '\n')
-        
+        for i in range(len(self.day_breakdown_list)):
+            self.day_breakdown_list[i] = self.day_breakdown_list[i].iloc[2:, :]
+
         return self.day_breakdown_list
         
-    def change_name_to_worker_id(self):
-        #Currently working on this part
-        print(self.workers_table)
-        
+    def __change_name_to_worker_id(self):
         id_name_dict = dict(zip(self.workers_table[:, 1], self.workers_table[:, 0]))
         
         for i in self.day_breakdown_list:
-            #print(i)
             i.iloc[:, 0] = i.iloc[:, 0].replace(id_name_dict)
-            
-        print(self.day_breakdown_list)
-                            
-    def get_preference_matrix(self):
-        for j in self.day_breakdown_list:
-            for i in j:
-                ...     
         
+
+    def __update_workers_table(self):
+        for day in self.day_breakdown_list:
+            array_df = pd.DataFrame(self.workers_table)
+            day = day.iloc[:, :2]
+            day = day.rename(columns={'Unnamed: 1': 0, 'Unnamed: 2': 2})
+            result = pd.merge(array_df, day.iloc[:, :2], on=0, how='left')
+            self.workers_table = result.to_numpy()
+
+        common_strings = np.array([next(x for x in row[2:] if not pd.isna(x)) for row in self.workers_table])
+        self.workers_table = self.workers_table[:, :2]
+        self.workers_table = np.column_stack((self.workers_table, common_strings))
+        return self.workers_table
+    
+    def __get_preference_matrix(self):
+        self.preference_matrix = np.full((self.num_workers, self.num_days, self.num_shifts), self.large_num)
+        
+        for j in range(self.num_days):
+            for index in range(len(self.day_breakdown_list[j].iloc[:, 0])):
+                for k in range(self.num_shifts):
+                    i = self.day_breakdown_list[j].iloc[index, 0]
+                    preference_ijk = self.day_breakdown_list[j].iloc[index, 2+k]
+                    preference_ijk = self.large_num if (pd.isna(preference_ijk) or str(preference_ijk).isspace()) else int(preference_ijk)
+                    self.preference_matrix[i, j, k] = preference_ijk
+        
+        return self.preference_matrix
+    
 if __name__=="__main__":
     ep = Excel_Parser("MTC Availability Template.xlsx")
-    df = ep.parse_day_breakdown()
-     
+    student_workers, x_ijk = ep.get_all_data()
+    print(student_workers)
+    print(x_ijk)
     
-    workers_table = ep.get_workers_table()
-    #preference_matrix = ep.get_preference_matrix()
-    
-    
-    #print(ep.num_workers())
-    
-    #x_ijk = ep.get_preference_matrix()
-    #print(x_ijk.shape)
-    
-    ep.change_name_to_worker_id()
--- solution.py
+++ solution.py
@@ -3,53 +3,65 @@
 from Excel_Parser import *
 from Qualtrics_Parser import *
 from pulp import *
-from gurobipy import *
+#from gurobipy import *
 
 def get_data():
     ...
     
 def compute_solution():
     scheduler_model = LpProblem("MTC Scheduler", LpMinimize)
+    ep = Excel_Parser("MTC Availability Template.xlsx")
+    student_workers, x_ijk = ep.get_all_data()
+    i, j, k = x_ijk.shape
     
-    student_workers = ...
-    x_ijk = ...
-    l_ijk = ...
-    u_ijk = ...
+    l_ijk = np.full((i, j, k), 2)
+    u_ijk = np.full((i, j, k), 5)
     
+    #Friday 2pm-6pm unavailable so change l_ijk and u_ijk to 0 for those
+    l_ijk[:, 4, 4:] = 0
+    u_ijk[:, 4, 4:] = 0
+    
+
     PLAs = student_workers[np.where(student_workers[:, 2] == "PLA")]
     GLAs = student_workers[np.where(student_workers[:, 2] == "GLA")]
-    TAs = student_workers[np.where(student_workers[:, 2] == "GLA")]
+    TAs = student_workers[np.where(student_workers[:, 2] == "TA")]
+    
+    print(PLAs)
+    print(GLAs)
+    print(TAs)
+    
     
     ### Initialize Decision Variables ###
     a_ijk = {}
+    varsKey = {}
     for i in range(x_ijk.shape[0]):
         for j in range(x_ijk.shape[1]):
             for k in range(x_ijk.shape[2]):
-                a_ijk[(i, j, k)] = LpVariable(name=f"a({i}, {j}, {k})", lowBound=0, upBound=1, cat=LpBinary)
-                
+                a_ijk[(i, j, k)] = LpVariable(name=f"a({i},{j},{k})", lowBound=0, upBound=1, cat=LpBinary)
+                varsKey[f"a({i},{j},{k})"] = (i, j, k)
     
         
     ### Objective Function ###
-    scheduler_model += lpSum([x_ijk[i, j, k]*a_ijk[i][j][k]
-                                for i in student_workers[0]
+    scheduler_model += lpSum([x_ijk[i, j, k]*a_ijk[i, j, k]
+                                for i in student_workers[:, 0]
                                 for j in range(x_ijk.shape[1])
                                 for k in range(x_ijk.shape[2])])
     
     ### Constraints ###
 
     ## PLAs work one shift ##
-    for i in PLAs[0]:
+    for i in PLAs[:, 0]:
         scheduler_model += (
-                    lpSum([a_ijk[i][j][k]
+                    lpSum([a_ijk[i, j, k]
                     for j in range(x_ijk.shape[1])
                     for k in range(x_ijk.shape[2])]) == 1 
             )
     
     ## GLAs and TAs work two shifts ##
     
-    for i in np.vstack((GLAs, TAs)):
+    for i in np.vstack((GLAs, TAs))[:, 0]:
         scheduler_model += (
-                    lpSum([a_ijk[i][j][k]
+                    lpSum([a_ijk[i, j, k]
                         for j in range(x_ijk.shape[1])
                         for k in range(x_ijk.shape[2])]) == 2 
             )
@@ -59,14 +71,22 @@ def compute_solution():
     for j in range(x_ijk.shape[1]):
         for k in range(x_ijk.shape[2]):
             scheduler_model += (
-                    lpSum([a_ijk[i][j][k] 
-                        for i in student_workers[0]]) >= l_ijk[i, j, k]
+                    lpSum([a_ijk[i, j, k] 
+                        for i in student_workers[:, 0]]) >= l_ijk[i, j, k]
             )
             
     for j in range(x_ijk.shape[1]):
         for k in range(x_ijk.shape[2]):
             scheduler_model += (
-                    lpSum([a_ijk[i][j][k] 
-                        for i in student_workers[0]]) <= u_ijk[i, j, k]
+                    lpSum([a_ijk[i, j, k]
+                        for i in student_workers[:, 0]]) <= u_ijk[i, j, k]
             )
     
+    scheduler_model.solve()
+    print(LpStatus[scheduler_model.status])
+    print(scheduler_model.objective.value())
+    
+    toReturn = [v for v in scheduler_model.variables() if all((v.varValue is not None, v.varValue > 1e-6))]
+    print(toReturn)
+
+compute_solution()    
