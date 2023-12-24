ac01e837b436fe716d45a472b18b4275b0deb13d
ArjunVenat <arjunvenat@gmail.com>
2023-12-24 11:33:23 -0500

    minor refactoring changes to solution file

--- solution.py
+++ solution.py
@@ -5,22 +5,28 @@ from Qualtrics_Parser import *
 from pulp import *
 #from gurobipy import *
 
-def get_data():
-    ...
+def get_data(mode, file_path=""):
+    #mode is either Excel or Qualtrics
+    if (mode == "Excel"):
+        ep = Excel_Parser(file_path)
+        student_workers, x_ijk = ep.get_all_data()
+        i, j, k = x_ijk.shape
+        l_ijk = np.full((i, j, k), 2)
+        u_ijk = np.full((i, j, k), 5)
     
+        #Friday 2pm-6pm unavailable so change l_ijk and u_ijk to 0 for those
+        l_ijk[:, 4, 4:] = 0
+        u_ijk[:, 4, 4:] = 0
+        
+        return (student_workers, x_ijk, l_ijk, u_ijk)
+    
+    if (mode == "Qualtrics"):
+        ...
+        
 def compute_solution():
     scheduler_model = LpProblem("MTC Scheduler", LpMinimize)
-    ep = Excel_Parser("MTC Availability Template.xlsx")
-    student_workers, x_ijk = ep.get_all_data()
-    i, j, k = x_ijk.shape
-    
-    l_ijk = np.full((i, j, k), 2)
-    u_ijk = np.full((i, j, k), 5)
-    
-    #Friday 2pm-6pm unavailable so change l_ijk and u_ijk to 0 for those
-    l_ijk[:, 4, 4:] = 0
-    u_ijk[:, 4, 4:] = 0
-    
+
+    student_workers, x_ijk, l_ijk, u_ijk = get_data(mode="Excel", file_path="MTC Availability Template.xlsx")
 
     PLAs = student_workers[np.where(student_workers[:, 2] == "PLA")]
     GLAs = student_workers[np.where(student_workers[:, 2] == "GLA")]
@@ -83,10 +89,16 @@ def compute_solution():
             )
     
     scheduler_model.solve()
-    print(LpStatus[scheduler_model.status])
-    print(scheduler_model.objective.value())
-    
-    toReturn = [v for v in scheduler_model.variables() if all((v.varValue is not None, v.varValue > 1e-6))]
-    print(toReturn)
+    num_j = x_ijk.shape[1]
+    num_k = x_ijk.shape[2]
+    result_array = [[[] for _ in range(num_k)] for _ in range(num_j)]
 
-compute_solution()    
+    for v in scheduler_model.variables():
+        if v.varValue is not None and v.varValue > 1e-6:
+            i, j, k = varsKey[v.name]
+            result_array[j][k].append(i)
+        
+    for j in range(len(result_array)):
+        print(f"{result_array[j]}")    
+    
+compute_solution()
