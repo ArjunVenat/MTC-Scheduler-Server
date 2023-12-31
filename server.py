0108d2a7569c3ede66d0665e01169d357faf28ea
ArjunVenat <arjunvenat@gmail.com>
2023-12-31 14:45:00 -0500

    documentation changes and slight change to formulation

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
-##Uses Flask to handle the HTTP requests, allowing client side and server side communication
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from solution import *
 
-import flask
+app = Flask(__name__)
+CORS(app)
+
+#Get optimal schedule to be displayed on React client-side
+@app.route('/api/display_solution', methods=['GET'])
+def display_solution():
+    student_workers, x_ijk, l_ijk, u_ijk = get_data("Excel", "MTC Availability Template.xlsx")
+    solution = compute_solution(student_workers, x_ijk, l_ijk, u_ijk)
+    data = {"solution": solution}
+    return jsonify(data)
+if __name__ == '__main__':
+    app.run()
--- solution.py
+++ solution.py
@@ -23,10 +23,23 @@ def get_data(mode, file_path=""):
     if (mode == "Qualtrics"):
         ...
         
-def compute_solution():
+def compute_solution(student_workers, x_ijk, l_ijk, u_ijk):
+    ## student_workers is an NumPy Array with shape (n, 5), where n is the number of student workers
+    # column 1 is the index in the table corresponding to that worker: int in range [0, n)
+    # column 2 is the name of the worker: str
+    # column 3 is the type of the workers (PLA/GLA/TA): str
+    # column 4 is the number of hours that the worker wants to work (the default is 1 for PLAs and 2 for GLAs/TAs unless additional hours were specified)
+    # column 5 is their shift preference for the workers (NaN if 1 hour, Back-To-Back, Same-Day): str
+    
+    ## x_ijk is a NumPy array with shape (n, d, s), where n is the number of student workers, d is the number of days, and s is the number of shifts
+    # Each element represents worker i's preference for working in day j shift k (X, 1, 2, 3), where X indicates that they are unavailable, and is a large number (in this case it is set to 10000)
+    # 1, 2, and 3 are in order of most preferable to least preferable: int
+    
+    ## l_ijk, u_ijk are NumPy arrays with shape (d, s)
+    # l_ijk is the minimum number of student workers required on day j shift k: int
+    # u_ijk is the maximum number of student workers required on day j shift k: int
+    
     scheduler_model = LpProblem("MTC Scheduler", LpMinimize)
-
-    student_workers, x_ijk, l_ijk, u_ijk = get_data(mode="Excel", file_path="MTC Availability Template.xlsx")
 
     PLAs = student_workers[np.where(student_workers[:, 2] == "PLA")]
     GLAs = student_workers[np.where(student_workers[:, 2] == "GLA")]
@@ -36,6 +49,9 @@ def compute_solution():
     print(GLAs)
     print(TAs)
     
+    ## Workers who prefer Back-To-Back Shifts ##
+    # Back_To_Back_Workers = student_workers[np.where(student_workers[:, 4] == "Back-To-Back")]
+    # Same_Day_Workers = student_workers[np.where(student_workers[:, 4] == "Same-Day")]
     
     ### Initialize Decision Variables ###
     a_ijk = {}
@@ -60,7 +76,7 @@ def compute_solution():
         scheduler_model += (
                     lpSum([a_ijk[i, j, k]
                     for j in range(x_ijk.shape[1])
-                    for k in range(x_ijk.shape[2])]) == 1 
+                    for k in range(x_ijk.shape[2])]) == 1 # Change from 1 to the worker's preferred number of hours (student_workers[i, 3])
             )
     
     ## GLAs and TAs work two shifts ##
@@ -69,7 +85,7 @@ def compute_solution():
         scheduler_model += (
                     lpSum([a_ijk[i, j, k]
                         for j in range(x_ijk.shape[1])
-                        for k in range(x_ijk.shape[2])]) == 2 
+                        for k in range(x_ijk.shape[2])]) == 2 # Change from 2 to the worker's preferred number of hours (student_workers[i, 3])
             )
     
     ## Set lower and upper bound on number of workers for each shift ##
@@ -88,6 +104,22 @@ def compute_solution():
                         for i in student_workers[:, 0]]) <= u_ijk[i, j, k]
             )
     
+    ## Handle Back-To-Back Shifts ##
+    """
+    for i in Back_To_Back_Workers[:, 0]:
+            scheduler_model += (
+                    lpSum(np.to_binary([a_ijk[i, j, :]) & (np.to_binary(a_ij) >> 1) for j in x_ijk.shape[1]]) > 0)
+        
+    """
+    #np.to_binary is not a real function, I am just using it as a placeholder for some function that converts the vector a_ijk[i, j, :] to a binary vector
+    
+    
+    ## Handle Same-Day Shifts ##
+    """for i in Same_Day_Workers[:, 0]:
+                scheduler_model += (
+                        lpSum( for j in x_ijk.shape[1]]) > 0)
+    """
+
     scheduler_model.solve()
     num_j = x_ijk.shape[1]
     num_k = x_ijk.shape[2]
@@ -96,9 +128,13 @@ def compute_solution():
     for v in scheduler_model.variables():
         if v.varValue is not None and v.varValue > 1e-6:
             i, j, k = varsKey[v.name]
+            worker_name = student_workers[np.where(student_workers[:, 0] == i)][0, 1]
+            #result_array[j][k].append(worker_name)
             result_array[j][k].append(i)
         
-    for j in range(len(result_array)):
-        print(f"{result_array[j]}")    
-    
-compute_solution()
+    return result_array
+
+if __name__=="__main__":
+    student_workers, x_ijk, l_ijk, u_ijk = get_data("Excel", "MTC Availability Template.xlsx")
+    solution = compute_solution(student_workers, x_ijk, l_ijk, u_ijk)
+    print(solution)
